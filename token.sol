1ecfb9974945a078698ceddd8a2c3ebdcf099d39
oyincode <oyindadavid18@gmail.com>
2019-08-26 23:52:17 +0100

    ownerOnly modifier added

--- token.sol
+++ token.sol
@@ -4,15 +4,21 @@ contract OyincodeToken{
     
     mapping(address => uint256) balanceOf;
     uint256 initialAmount; 
+    address owner;
     
+    modifier ownerOnly(){
+        require(owner == msg.sender, "Only Owner can transfer tokens");
+        _;
+    }
     constructor() public {
         balanceOf[msg.sender] = initialAmount;
     }
     
-    function transferToken(address _to, uint256 value) public {
-        if(balanceOf[msg.sender] < value)revert();
-        if(balanceOf[_to] + value < balanceOf[_to])revert();
-        balanceOf[msg.sender] -= value;
+    function transferToken(address payable _to, uint256 value) public ownerOnly {
+        if(balanceOf[owner] < value)revert("Owner's balance must not be less than value to be transferred");
+        if(balanceOf[_to] + value < balanceOf[_to])revert("Recipient's balance must be more than former state");
+        _to.transfer(value);
+        balanceOf[owner] -= value;
         balanceOf[_to] += value;
     }
 }
