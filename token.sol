734f67243fc7523032ce4a55b46f97477c942de9
oyincode <oyindadavid18@gmail.com>
2019-08-27 01:17:20 +0100

    view owner balance

--- token.sol
+++ token.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.0 <0.7.0;
 contract OyincodeToken{
     
     mapping(address => uint256) balanceOf;
-    uint256 initialAmount = 100; 
+    uint256 initialAmount = 100 wei; 
     address payable owner;
     
     modifier ownerOnly(){
@@ -15,14 +15,16 @@ contract OyincodeToken{
         balanceOf[owner] = initialAmount;
     }
     
+    // transfer token function
     function transferToken(address payable _to, uint256 value) public payable ownerOnly {
         if(balanceOf[owner] < value)revert("Owner's balance must not be less than value to be transferred");
         if(balanceOf[_to] + value < balanceOf[_to])revert("Recipient's balance must be more than former state");
-        balanceOf[owner] += value;
-        balanceOf[_to] -= value;
+        balanceOf[owner] -= value;
+        balanceOf[_to] += value;
     }
     
-    function viewOwner() public view returns(uint256 _owner){
-        _owner = balanceOf[owner];
+    // view owner balance function
+    function viewOwnerBalance() public view returns(uint256 _owner){
+        _owner = balanceOf[msg.sender];
     }
 }
