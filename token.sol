97d73aff8f92ea730647c03833b6f8bae938b99d
oyincode <oyindadavid18@gmail.com>
2019-08-29 11:57:34 +0100

    name, symbol and decimal of token sent

--- token.sol
+++ token.sol
@@ -1,40 +1,21 @@
 pragma solidity >=0.4.0 <0.7.0;
 
-contract OyincodeToken{
+contract Token {
+
+
+    string public constant name = "OyincodeToken";
+    string public constant symbol = "OTH";
+    uint8 public constant decimals = 0;
+
+    function totalSupply() public view returns (uint256);
+    function balanceOf(address tokenOwner) public view returns (uint);
+    function allowance(address tokenOwner, address spender)
+    public view returns (uint);
+    function transfer(address to, uint tokens) public returns (bool);
+    function approve(address spender, uint tokens)  public returns (bool);
+    function transferFrom(address from, address to, uint tokens) public returns (bool);
     
-    mapping(address => uint256) balanceOf;
-    uint256 initialAmount = 100 wei; 
-    address payable owner;
-    address payable recipient;
-    
-    modifier ownerOnly(){
-        require(owner == msg.sender, "Only Owner can transfer tokens");
-        _;
-    }
-    constructor() public {
-        owner = msg.sender;
-        balanceOf[owner] = initialAmount;
-    }
-    
-    // transfer token function
-    function transferToken(address payable _to, uint256 value) public payable ownerOnly {
-        // equate the global recipient address to the local (_to)
-        recipient = _to;
-        //conditions for token transfer
-        if(balanceOf[owner] < value)revert("Owner's balance must not be less than value to be transferred");
-        if(balanceOf[recipient] + value < balanceOf[recipient])revert("Recipient's balance must be more than former state");
-        //reduce the sender's balance
-        balanceOf[owner] -= value;
-        //increase the recipient's balance
-        balanceOf[recipient] += value;
-    }
-    
-    // view owner balance function
-    function viewOwnerBalance() public view returns(uint256 _owner){
-        _owner = balanceOf[msg.sender];
-    }
-    // view recipient balance function
-    function viewRecipientBalance() public view returns(uint256 _recipient){
-        _recipient = balanceOf[recipient];
-    }
+    // events
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Transfer(address indexed from, address indexed to,uint tokens);
 }
