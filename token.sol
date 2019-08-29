82315e085e16aa4c81863bd9dfbafabe7fb5dcdc
oyincode <oyindadavid18@gmail.com>
2019-08-29 12:47:02 +0100

    owner only modifier added

--- token.sol
+++ token.sol
@@ -5,6 +5,7 @@ contract Token {
     string public constant name = "OyincodeToken";
     string public constant symbol = "OTH";
     uint8 public constant decimals = 0;
+    uint public numOfTokens;
     uint256 totalSupply_;
     
     mapping(address => uint256) balances;
@@ -14,13 +15,32 @@ contract Token {
         total = totalSupply_;
         balances[msg.sender] = totalSupply_;
     }
+     
+    modifier ownerOnly(){
+        require(numOfTokens <= balances[msg.sender], "Only owner can perform this action");
+        _;
+    }
     
+    // get token supply
+    function totalSupply() public view returns (uint256) {
+      return totalSupply_;
+    }
+    // get balance of owner's account
+    function balanceOf(address tokenOwner) public view returns (uint) {
+      return balances[tokenOwner];
+    }
     
-    function totalSupply() public view returns (uint256);
-    function balanceOf(address tokenOwner) public view returns (uint);
+    //transfer number of tokens, only owner can perform this action
+    function transfer(address receiver, uint numTokens) public ownerOnly returns  (bool) {
+      numTokens = numOfTokens;
+      balances[msg.sender] = balances[msg.sender] - numTokens;
+      balances[receiver] = balances[receiver] + numTokens;
+      emit Transfer(msg.sender, receiver, numTokens);
+      return true;
+    }
+    
     function allowance(address tokenOwner, address spender)
     public view returns (uint);
-    function transfer(address to, uint tokens) public returns (bool);
     function approve(address spender, uint tokens)  public returns (bool);
     function transferFrom(address from, address to, uint tokens) public returns (bool);
     
