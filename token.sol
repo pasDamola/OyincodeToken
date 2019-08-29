8f51367ff4255d43f38e14e23bda1e4d5cfad64b
oyincode <oyindadavid18@gmail.com>
2019-08-29 15:42:13 +0100

    Standard Token Library

--- token.sol
+++ token.sol
@@ -1,14 +1,15 @@
 pragma solidity >=0.4.0 <0.7.0;
+import "node_modules/openzeppelin-solidity/contracts/token/StandardToken.sol";
 
-contract Token {
+contract Token is ERC20Detailed, ERC20 {
     
-    string public constant name = "OyincodeToken";
-    string public constant symbol = "OTH";
-    uint8 public constant decimals = 0;
+    // string public constant name = "OyincodeToken";
+    // string public constant symbol = "OTH";
+    // uint8 public constant decimals = 0;
     uint256 totalSupply_;
     
     mapping(address => uint256) balances;
-    mapping(address => uint256) allowed;
+    mapping(address => mapping (address => uint256)) allowed;
     
 
     
@@ -34,33 +35,35 @@ contract Token {
     //transfer number of tokens, only owner can perform this action
     function transfer(address receiver, uint numTokens) public returns (bool) {
       require(numTokens <= balances[msg.sender], "Owner's balance must be more than number of tokens being sent");
-      balances[msg.sender] = balances[msg.sender] - numTokens;
-      balances[receiver] = balances[receiver] + numTokens;
+      balances[msg.sender] = balances[msg.sender].add(numTokens);
+      balances[receiver] = balances[receiver].add(numTokens);
       emit Transfer(msg.sender, receiver, numTokens);
       return true;
     }
     
     // owner approves of delegate accounts to withdraw tokens
     function approve(address delegate, uint numTokens) public returns (bool) {
-      allowed[delegate] = numTokens;
+      allowed[msg.sender][delegate] = numTokens;
       emit Approval(msg.sender, delegate, numTokens);
       return true;
     }
     
     // Get Number of Tokens Approved for Withdrawal
-    function allowance(address delegate) public view returns (uint) {
-      return allowed[delegate];
+    function allowance(address owner, address delegate) public view returns (uint) {
+      return allowed[owner][delegate];
     }
     
     //Transfer Tokens by Delegate
     function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
       require(numTokens <= balances[msg.sender], "Owner's balance must be more than number of tokens being sent");
-      require(numTokens <= allowed[owner], "Delegate's balance must be more than the number of tokens being sent");
-      balances[owner] = balances[owner] - numTokens;
-      allowed[owner] = allowed[owner] - numTokens;
-      balances[buyer] = balances[buyer] + numTokens;
+      require(numTokens <= allowed[msg.sender][owner], "Delegate's balance must be more than the number of tokens being sent");
+      balances[owner] = balances[owner].sub(numTokens);
+      allowed[msg.sender][owner] = allowed[msg.sender][owner].sub(numTokens);
+      balances[buyer] = balances[buyer].add(numTokens);
       emit Transfer(owner, buyer, numTokens);
       return true;
     }
+    
+    
    
 }
